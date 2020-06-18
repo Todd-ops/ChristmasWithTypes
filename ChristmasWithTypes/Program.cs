bd9ffbff39a51f09f36d71d1016a06e01730a84a
Todd Patton <64681278+Todd-ops@users.noreply.github.com>
2020-06-18 13:12:28 -0500

    christmas

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ChristmasWithTypes
 {
@@ -8,16 +9,16 @@ namespace ChristmasWithTypes
         {
             var xmas = new Christmas();
 
-            var xmasDay = Christmas.Day.Thursday;
-
-            //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            var xmasDay = Christmas.Day.Friday;
+            
+             //TODO set Santa's name to Kris Kringle
+            xmas.Santa = "Kris Kringle" ;
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{"Bike", "Hat", "Hockey Stick" };
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
